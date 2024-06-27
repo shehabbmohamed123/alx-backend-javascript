24bb565555456cf35276b98f7040a46b4a34bfac
shehabbmohamed123 <shehab12p@gmail.com>
2024-06-27 07:43:50 -0700

    a

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -2,20 +2,18 @@ export default class HolbertonClass {
   constructor(size, location) {
     if (typeof size !== 'number') {
       throw new TypeError('Size must be a number');
-    }
-    if (typeof location !== 'string') {
+    } else if (typeof location !== 'string') {
       throw new TypeError('Location must be a string');
     }
+
     this._size = size;
     this._location = location;
   }
 
-  toString() {
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
     return this._location;
   }
-
-  valueOf() {
-    return this._size;
-  }
 }
-
